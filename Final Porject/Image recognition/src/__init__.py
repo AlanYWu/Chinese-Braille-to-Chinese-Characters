b954417ffe140b5cbf5c2d48483b72e0db57f597
Alan WuYou <88386790+AlanYWu@users.noreply.github.com>
2024-01-19 22:24:50 +0800

    Delete Final Porject directory

--- Final Porject/Image recognition/src/__init__.py
+++
@@ -1,1 +0,0 @@
-from src.convert import *
--- Final Porject/Image recognition/src/utils/__init__.py
+++
@@ -1,4 +0,0 @@
-from utils.angelina_utils import *
-from utils.dsbi_utils import *
-from utils.kaggle_utils import *
-from utils.braille_natural_utils import *
